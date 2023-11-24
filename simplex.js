e28a5438f91d5d2fedf92362fd27313fbaba23a6
Michel de Castro Sunsin <mintchel@gmail.com>
2023-11-24 11:14:55 -0300

    Mostrar resultado de solução viável

--- simplex.js
+++ simplex.js
@@ -1,4 +1,21 @@
 const M = 'M'
+
+/*
+  Defina os valores de c, A e b
+  Importante: O problema precisa estar na forma padrão de minimização
+
+  op: Se a função objetivo é de minimização ou maximização (atribua 'min' ou 'max')
+  c: Vetor de coeficientes da função objetivo
+  A: Matriz de coeficientes das restrições
+  b: Vetor de termos independentes das restrições
+
+  O programa utilizará o método simplex de duas fases se o vetor de custo
+  c contiver algum valor M. Caso contrário, o programa utilizará o método
+  simplex padrão.
+*/
+
+// Exemplo de duas fases com solução ótima
+const op = 'max'
 let c = [-1, -1, 0, 0, 0, 0, M, M]
 
 let A = [
@@ -10,11 +27,19 @@ let A = [
 
 let b = [2, 4, 2, 4]
 
+// Exemplo de uma fase com solução ótima
+// const op = 'min'
+// let c = [-1, -1, 0, 0]
+// let A = [
+//   [24, 16, 1, 0],
+//   [5, 10, 0, 1],
+// ]
+// let b = [96, 45]
+
 /* 
-  Método que contrói o vetor de custo, sem os custos artificiais
+  Método que contrói o vetor de custo sem os custos artificiais
   e o vetor de custo artificial
 */
-
 function construirVetoresDeCusto() {
   // Vetor custo sem variáveis artificiais
   const cSemArtificiais = c.map((x) => {
@@ -49,10 +74,7 @@ function construirVetorDeBasesIniciais() {
       indiceColuna++
     ) {
       if (A[indiceLinha][indiceColuna] === 1) {
-        vetorDeBasesIniciais.push({
-          indice: indiceColuna,
-          valor: A[indiceLinha][indiceColuna],
-        })
+        vetorDeBasesIniciais.push(indiceColuna)
       }
     }
 
@@ -69,7 +91,7 @@ function construirVetorCustosReduzidos(vetorCusto, bases) {
     let custoReduzido = 0
 
     for (let indiceLinha = 0; indiceLinha < A.length; indiceLinha++) {
-      const indiceVetorCusto = bases[indiceLinha].indice
+      const indiceVetorCusto = bases[indiceLinha]
 
       custoReduzido +=
         vetorCusto[indiceVetorCusto] * A[indiceLinha][indiceColuna]
@@ -118,24 +140,32 @@ function atualizaMatrizAeVetorB(indiceLinhaPivo, indiceColunaPivo) {
   let novaA = [...A]
   let novoB = [...b]
 
-  const pivo = novaA[indiceLinhaPivo][indiceColunaPivo]
+  const linhaPivo = [...novaA[indiceLinhaPivo]]
+  const pivo = linhaPivo[indiceColunaPivo]
+  const bLinhaPivo = novoB[indiceLinhaPivo]
 
+  /*
+    Calcula as novas linhas da matriz A e o novo vetor b
+    utilizando a linha pivo
+  */
   for (let indiceLinha = 0; indiceLinha < novaA.length; indiceLinha++) {
-    if (indiceLinha !== indiceLinhaPivo) {
-      const coeficienteLinear = novaA[indiceLinha][indiceColunaPivo] / pivo
+    const coeficienteLinear = novaA[indiceLinha][indiceColunaPivo] / pivo
 
-      for (
-        let indiceColuna = 0;
-        indiceColuna < novaA[0].length;
-        indiceColuna++
-      ) {
+    for (let indiceColuna = 0; indiceColuna < novaA[0].length; indiceColuna++) {
+      if (indiceLinha === indiceLinhaPivo) {
+        novaA[indiceLinha][indiceColuna] =
+          novaA[indiceLinha][indiceColuna] / pivo
+      } else {
         novaA[indiceLinha][indiceColuna] =
           novaA[indiceLinha][indiceColuna] -
-          coeficienteLinear * novaA[indiceLinhaPivo][indiceColuna]
+          coeficienteLinear * linhaPivo[indiceColuna]
       }
+    }
 
-      novoB[indiceLinha] =
-        novoB[indiceLinha] - coeficienteLinear * novoB[indiceLinhaPivo]
+    if (indiceLinha === indiceLinhaPivo) {
+      novoB[indiceLinha] = novoB[indiceLinha] / pivo
+    } else {
+      novoB[indiceLinha] = novoB[indiceLinha] - coeficienteLinear * bLinhaPivo
     }
   }
 
@@ -145,10 +175,7 @@ function atualizaMatrizAeVetorB(indiceLinhaPivo, indiceColunaPivo) {
 function atualizaBases(indiceLinhaPivo, indiceColunaPivo, bases) {
   const novasBases = [...bases]
 
-  novasBases[indiceLinhaPivo] = {
-    indice: indiceColunaPivo,
-    valor: A[indiceLinhaPivo][indiceColunaPivo],
-  }
+  novasBases[indiceLinhaPivo] = indiceColunaPivo
 
   return novasBases
 }
@@ -159,7 +186,6 @@ function removerColunasVariaveisArtificiais(A, custosReduzidos) {
 
   while (c.some((x) => x === M)) {
     const indiceColuna = c.findIndex((x) => x === M)
-    console.log(indiceColuna)
 
     aSemColunasArtificiais.forEach((row, index) => {
       row.splice(indiceColuna, 1)
@@ -181,6 +207,7 @@ function simplex(c, A, b, custosReduzidosPreviamenteCalculados, bases) {
     custosReduzidos = construirVetorCustosReduzidos(c, bases)
   }
 
+  let contadorIteracao = 1
   while (custosReduzidos.some((x) => x < 0)) {
     const indiceColunaPivo = verificaQuemSaiDaBase(custosReduzidos)
     const indiceLinhaPivo = verificaQuemEntraNaBase(indiceColunaPivo)
@@ -194,15 +221,41 @@ function simplex(c, A, b, custosReduzidosPreviamenteCalculados, bases) {
 
     A = [...novaA]
     b.forEach((_, index) => (b[index] = novoB[index]))
-    bases = [...novasBases]
+    bases.forEach((_, index) => (bases[index] = novasBases[index]))
 
     custosReduzidos = construirVetorCustosReduzidos(c, bases)
+
+    // Mostra como está o quadro simplex em cada iteração
+    const basesConvertidas = []
+    bases.forEach((x) => basesConvertidas.push(`x${x + 1}`))
+    console.log(`Iteração ${contadorIteracao}`)
+    console.log('A: ', A)
+    console.log('b: ', b)
+    console.log('Base: ', basesConvertidas)
+    console.log('cr: ', custosReduzidos)
+
+    contadorIteracao++
   }
 
-  console.log('A: ', A)
-  console.log('b: ', b)
-  console.log('bases: ', bases)
-  console.log('cr: ', custosReduzidos)
+  // Cálculo do x* e z*
+  const xOtimo = []
+  bases.forEach((x, index) => {
+    let resultado = c[x] * b[index]
+
+    if (op === 'max') {
+      resultado *= -1
+    }
+
+    if (resultado !== 0) {
+      xOtimo.push(resultado)
+    }
+  })
+
+  let z = xOtimo.reduce((acc, x) => acc + x, 0)
+
+  console.log('Solução viável encontrada!')
+  console.log('x*: ', xOtimo)
+  console.log('z*: ', z)
 }
 
 /*
@@ -221,45 +274,56 @@ function simplexDuasFases(c, A, b) {
     bases,
   )
 
+  /*
+    Só vai entrar no método de duas fases se o vetor de coeficientes da função objetivo
+    contiver algum valor M
+  */
+  const utilizarSimplexDuasFases = c.some((x) => x === M)
   let contadorIteracao = 1
-  while (custosReduzidosArtificial.some((x) => x < 0)) {
-    const indiceColunaPivo = verificaQuemSaiDaBase(custosReduzidosArtificial)
-    const indiceLinhaPivo = verificaQuemEntraNaBase(indiceColunaPivo)
 
-    let { novaA, novoB } = atualizaMatrizAeVetorB(
-      indiceLinhaPivo,
-      indiceColunaPivo,
-    )
+  if (utilizarSimplexDuasFases) {
+    while (custosReduzidosArtificial.some((x) => x < 0)) {
+      const indiceColunaPivo = verificaQuemSaiDaBase(custosReduzidosArtificial)
+      const indiceLinhaPivo = verificaQuemEntraNaBase(indiceColunaPivo)
 
-    let novasBases = atualizaBases(indiceLinhaPivo, indiceColunaPivo, bases)
+      let { novaA, novoB } = atualizaMatrizAeVetorB(
+        indiceLinhaPivo,
+        indiceColunaPivo,
+      )
 
-    A = [...novaA]
-    b.forEach((_, index) => (b[index] = novoB[index]))
-    bases = [...novasBases]
+      let novasBases = atualizaBases(indiceLinhaPivo, indiceColunaPivo, bases)
 
-    custosReduzidos = construirVetorCustosReduzidos(cSemArtificiais, bases)
-    custosReduzidosArtificial = construirVetorCustosReduzidos(
-      cArtificial,
-      bases,
-    )
+      A = [...novaA]
+      b.forEach((_, index) => (b[index] = novoB[index]))
+      bases.forEach((_, index) => (bases[index] = novasBases[index]))
 
-    console.log(`Iteração ${contadorIteracao}`)
-    console.log('A: ', A)
-    console.log('b: ', b)
-    console.log('bases: ', bases)
-    console.log('cr: ', custosReduzidos)
-    console.log('cra: ', custosReduzidosArtificial)
+      custosReduzidos = construirVetorCustosReduzidos(cSemArtificiais, bases)
+      custosReduzidosArtificial = construirVetorCustosReduzidos(
+        cArtificial,
+        bases,
+      )
 
-    contadorIteracao++
+      // Mostra como está o quadro simplex em cada iteração
+      const basesConvertidas = []
+      bases.forEach((x) => basesConvertidas.push(`x${x + 1}`))
+      console.log(`Iteração ${contadorIteracao}`)
+      console.log('A: ', A)
+      console.log('b: ', b)
+      console.log('Base: ', basesConvertidas)
+      console.log('cr: ', custosReduzidos)
+      console.log('cra: ', custosReduzidosArtificial)
+
+      contadorIteracao++
+    }
+
+    console.log('Fim da primeira fase. Iniciando segunda fase...')
+
+    let { aSemColunasArtificiais, custosReduzidosSemColunasArtificiais } =
+      removerColunasVariaveisArtificiais(A, custosReduzidos)
+
+    A = [...aSemColunasArtificiais]
+    custosReduzidos = [...custosReduzidosSemColunasArtificiais]
   }
-
-  console.log('Fim da primeira fase. Iniciando segunda fase...')
-
-  let { aSemColunasArtificiais, custosReduzidosSemColunasArtificiais } =
-    removerColunasVariaveisArtificiais(A, custosReduzidos)
-
-  A = [...aSemColunasArtificiais]
-  custosReduzidos = [...custosReduzidosSemColunasArtificiais]
 
   simplex(c, A, b, custosReduzidos, bases)
 }
